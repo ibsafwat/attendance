d7c14f937fa2177694b1040d27c958cfda8b44bc
Ibrahim Safwat <isafwat@sumerge.com>
2013-11-21 21:59:58 +0200

    Added Symfony files and created Bundle

---
+++ src/Sumerge/AttendanceBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sumerge\AttendanceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SumergeAttendanceBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Sumerge/AttendanceBundle/SumergeAttendanceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sumerge\AttendanceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SumergeAttendanceBundle extends Bundle
+{
+}
